5042516048e2ac06594d2afc7839a22aeae3e35b
Piyush Gupta <116789485+Piyuzh77@users.noreply.github.com>
2025-09-20 18:35:09 +0530

    Updated for HTML like layout

--- src/main/java/com/movierRando/MovieRando/Controllers/HomeController.java
+++ src/main/java/com/movierRando/MovieRando/Controllers/HomeController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "🎬 Welcome to 🎥MovieRando📽️!!! Your gateway to SERIOUS!! movie recommendations. \n \n TRY /media/random to get a random MOVIE recommendation or use /media/random/?type=tv to get a random TV SHOW recommendation.!! to know more visit https://github.com/Piyuzh77/MovieRando-SpringBoot ";
+   @GetMapping(value = "/", produces = "text/html")
+public String home() {
+    return """
+           🎬 Welcome to 🎥MovieRando📽️!!! Your gateway to SERIOUS!! movie recommendations.
+           <br><br>
+           TRY /media/random to get a random MOVIE recommendation or use /media/random/?type=tv to get a random TV SHOW recommendation.!!
+           <br><br>
+           To know more visit <a href="https://github.com/Piyuzh77/MovieRando-SpringBoot">Here!!</a>
+           """;
     }
 }
